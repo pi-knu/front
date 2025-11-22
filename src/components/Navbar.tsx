840c0960c6da864bc7583a1d63f26038fbb309f2
NastyaPashko <nastya1200526@gmail.com>
2025-11-22 14:20:04 +0200

    feat: add responsive-mobile Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,109 @@
+import { Input } from "@/components/ui/input"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Search } from "lucide-react";
+
+import {
+    Gavel,
+    Store,
+    Bell,
+    ShoppingCart,
+    User,
+    Menu,
+    X
+} from "lucide-react";
+
+function Navbar() {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <nav className="w-full border-b bg-white px-6 py-3 flex items-center justify-between">
+
+            <div className="flex items-center gap-4">
+                <button
+                    className="lg:hidden flex items-center"
+                    onClick={() => setOpen(!open)}
+                >
+                    {open ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+                </button>
+
+                <div className="w-8 h-8 bg-gray-300 rounded flex items-center justify-center">
+                    <Gavel className="w-4 h-4 text-white" />
+                </div>
+
+                <Link to="/" className="text-xl font-semibold">
+                    AuctionHub
+                </Link>
+
+                <div className="hidden lg:flex items-center gap-4">
+                    <Link to="/marketplace" className="text-gray-500 p-2 flex items-center gap-1 text-sm hover:bg-gray-100 hover:text-gray-900  transition rounded-lg">
+                        <Store className="w-4 h-4 text-gray-500" />
+                        Marketplace
+                    </Link>
+
+                    <Link
+                        to="/auctions"
+                        className="flex items-center gap-2 text-gray-500 px-4 py-2  text-sm hover:bg-gray-100 hover:text-gray-900 transition rounded-lg"
+                    >
+                        <Gavel className="w-4 h-4" />
+                        Auctions
+                        <span className="text-xs bg-white text-black px-2 py-0.5 rounded">
+                            Live
+                        </span>
+                    </Link>
+
+                    <div className="relative w-70 ml-8">
+                        <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 w-4 h-4" />
+
+                        <Input
+                            placeholder="Search products, auctions..."
+                            className="bg-white pl-10"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="hidden lg:flex items-center gap-10">
+
+
+                <Bell className="w-5 h-5" />
+                <ShoppingCart className="w-5 h-5" />
+
+                <Link to="/profile" className="flex items-center gap-1">
+                    <User className="w-5 h-5" /> Profile
+                </Link>
+            </div>
+            {open && (
+                <div className="absolute top-16 left-0 w-full bg-white border-b shadow-lg flex flex-col gap-4 px-6 py-4 lg:hidden">
+
+                    <Input placeholder="Search..." className="bg-white" />
+
+                    <Link to="/marketplace" className="flex items-center gap-2 text-gray-500">
+                        <Store className="w-4 h-4" /> Marketplace
+                    </Link>
+
+                    <Link
+                        to="/auctions"
+                        className="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg w-fit"
+                    >
+                        <Gavel className="w-4 h-4" />
+                        Auctions
+                        <span className="text-xs bg-white text-black px-2 py-0.5 rounded">
+                            Live
+                        </span>
+                    </Link>
+
+                    <div className="flex items-center gap-4">
+                        <Bell className="w-5 h-5" />
+                        <ShoppingCart className="w-5 h-5" />
+                        <Link to="/profile" className="flex items-center gap-1">
+                            <User className="w-5 h-5" /> Profile
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,6 +1,11 @@
+
+import Navbar from "@/components/Navbar";
+
 function HomePage() {
   return (
     <div>
+      <Navbar />
+
       <h1>Home Page</h1>
       <p>Welcome to the website!</p>
     </div>
